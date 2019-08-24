9c947a3b90783da46b6a2a213b8eca8ece0ee5e8
caposcar1998 <42816786+caposcar1998@users.noreply.github.com>
2019-08-23 22:53:51 -0500

    Web page
    Saveing files

---
+++ Web/querry/querryIndex.js
@@ -0,0 +1,14 @@
+$(document).ready(function(){
+
+   $('.presentar').slick({
+       dots:true,
+       autoplay:1000,
+       infinite:true,
+       slidesToShow: 2
+   });
+    
+   
+    
+    
+});
+
